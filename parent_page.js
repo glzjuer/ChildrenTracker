f9faaea482b09d55f09c61af17a818bfdb1f60d9
colson0804 <craigolson2015@u.northwestern.edu>
2014-05-09 13:07:29 -0500

    Sample child code created

--- parent_page.js
+++ parent_page.js
@@ -1,43 +1,54 @@
-  function initialize() {
+var initialLocation;
+var tech = new google.maps.LatLng(42.057807,-87.675765);
+var browserSupportFlag =  new Boolean();
 
-     var tech = new google.maps.LatLng(42.057807,-87.675765);
+function initialize() {
+  var myOptions = {
+    zoom: 18,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), myOptions);
 
-      var mapOptions = {
-        zoom: 18,
-        center: tech
-      };
-
-      var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
-
-          // Try W3C Geolocation (Preferred)
-      if(navigator.geolocation) {
-        browserSupportFlag = true;
-        navigator.geolocation.getCurrentPosition(function(position) {
-          initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-          map.setCenter(initialLocation);
-        }, function() {
-          handleNoGeolocation(browserSupportFlag);
-        });
-      }
-      // Browser doesn't support Geolocation
-      else {
-        browserSupportFlag = false;
-        handleNoGeolocation(browserSupportFlag);
-      }
-
-    function handleNoGeolocation(errorFlag) {
-      if (errorFlag == true) {
-        alert("Geolocation service failed.");
-        initialLocation = tech;
-      } else {
-        alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
-        initialLocation = tech;
-      }
+  // Try W3C Geolocation (Preferred)
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
       map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  }
+  // Browser doesn't support Geolocation
+  else {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
+
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      alert("Geolocation service failed.");
+    } else {
+      alert("Your browser doesn't support geolocation.");
     }
+    initialLocation = tech;
+    map.setCenter(initialLocation);
+  }
+}
 
-      var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+//google.maps.event.addDomListener(window, 'load', initialize);
 
-    }
+// Add an item to the dropdown list
+var addItem = function(item) {
+  $("drop_list").append('<li data-filtertext="'+item+'">'+item+'</li>');
+}
 
-    //google.maps.event.addDomListener(window, 'load', initialize);
+// Modal animation
+$("#save").click(function() {
+      event.preventDefault();
+      
+      $('.to_hide').slideUp(400,function(){
+        $('.to_show').slideDown();
+      });
+    return false;
+});
